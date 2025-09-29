ebf3f29bc6fa2439a60ea91c67f4f8e2c4fda668
perfectoMIA <maremyanov.ia@students.dvfu.ru>
2025-09-29 20:21:22 +1000

    добавил функцию возвращения в меню тега

--- bot/passive_functions.py
+++ bot/passive_functions.py
@@ -1,9 +1,12 @@
 import asyncio
 from datetime import date, datetime
 from aiogram import Bot
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
 
 from models import DataBase
 from config import ADMINS_ID
+from keyboards import inline as inline_keyborads
 
 
 # отравка сообщений админам о скором дне рождения
@@ -17,3 +20,22 @@ async def birthday_notice(bot: Bot):
             for ID in admins_id:
                 await bot.send_message(chat_id=ID, text=f"через 7 дней день рождения у: @{person[0]}")
     await asyncio.sleep(86400)  # раз в сутки запускается
+
+
+async def back_to_tag_information(tag_name: str, message: Message, state: FSMContext):
+    markup = inline_keyborads.get_tag_information_keyboard(tag_name)
+    description = DataBase.Get_tag_description(tag_name)[0][0]
+    list_users = DataBase.Get_users_from_tag(tag_name)
+    users = ""
+    for i in range(len(list_users)):
+        users += "@" + " ".join(list(list_users[i]))
+        users += "\n"
+    await message.bot.edit_message_text(chat_id=message.chat.id, message_id=(await state.get_data())["message_id"],
+                                        text=f"Название тега: {tag_name}\n"
+                                             f"Описание тега: {str(description)}\n"
+                                             f"Участники тега:\n{users}",
+                                        reply_markup=markup)
+
+
+def sort_birthday(user: tuple):
+    return datetime.strptime(user[1], "%d.%m.%Y")
