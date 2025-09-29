eaa47870fb9bfca5fa1a551c2230feed0a507afe
perfectoMIA <maremyanov.ia@students.dvfu.ru>
2025-09-29 20:24:33 +1000

    добавил клавиатуры: с пользователями в списке, возможностями изменить тег, с возвратом к настройкам тега, возможностями изменить пользователей в теге и добавлением и удалением из тега

--- bot/keyboards/inline.py
+++ bot/keyboards/inline.py
@@ -35,14 +35,12 @@ def get_tag_information_keyboard(tag_name: str) -> InlineKeyboardMarkup:
 # ближайшие к дню рождения люди
 def get_birth_date_keyboard(persons: list) -> InlineKeyboardMarkup:
     name = ""
-    print(persons)
     for i in range(len(persons)):
         name += f"@{persons[i]}"
         if i + 1 < len(persons):
             name += " и "
-    print(name)
     markup = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=f"Написать информацию по дню рождения {name}",
+        [InlineKeyboardButton(text=f"Написать о дне рождении {name}",
                               callback_data=f"message_about_birthday {name}")],
         [InlineKeyboardButton(text="Назад", callback_data="start")]
     ])
@@ -51,7 +49,7 @@ def get_birth_date_keyboard(persons: list) -> InlineKeyboardMarkup:
 
 # прекращение создания тега
 def stop_creating_tag() -> InlineKeyboardMarkup:
-    markup = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="прекратить создание тега",
+    markup = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="отменить создание тега",
                                                                          callback_data="stop_creating_tag")]])
     return markup
 
@@ -64,7 +62,18 @@ def get_all_name_users() -> InlineKeyboardMarkup:
         markup.inline_keyboard.append([InlineKeyboardButton(text=f"@{user[0]}", callback_data=f"user {user[0]}")])
     markup.inline_keyboard.append([InlineKeyboardButton(text="закончить выбор",
                                                         callback_data="finish_create_tag")])
-    markup.inline_keyboard.append([InlineKeyboardButton(text="прекратить создание тега",
+    markup.inline_keyboard.append([InlineKeyboardButton(text="отменить создание тега",
+                                                        callback_data="stop_creating_tag")])
+    return markup
+
+
+def get_users_in_list(list_users: list):
+    markup = InlineKeyboardMarkup(inline_keyboard=[[]])
+    for user in list_users:
+        markup.inline_keyboard.append([InlineKeyboardButton(text=f"@{user[0]}", callback_data=f"user {user[0]}")])
+    markup.inline_keyboard.append([InlineKeyboardButton(text="закончить выбор",
+                                                        callback_data="finish_create_tag")])
+    markup.inline_keyboard.append([InlineKeyboardButton(text="отменить создание тега",
                                                         callback_data="stop_creating_tag")])
     return markup
 
@@ -73,11 +82,57 @@ def get_all_name_users() -> InlineKeyboardMarkup:
 def choice_of_life_tag(tag_name: str) -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Да", callback_data=f"accept_delete {tag_name}")],
-        [InlineKeyboardButton(text="Нет", callback_data=f"back_to_all_tags")]
+        [InlineKeyboardButton(text="Нет", callback_data=f"tag information {tag_name}")]
     ])
     return markup
 
 
 def accept_event() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="ОК", callback_data="start")]])
+    return markup
+
+
+def get_tag_settings(tag_name: str) -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Изменить название тега", callback_data=f"edit_name {tag_name}")],
+        [InlineKeyboardButton(text="Изменить описание тега", callback_data=f"edit_description {tag_name}")],
+        [InlineKeyboardButton(text="Изменить участников", callback_data=f"edit_users {tag_name}")],
+        [InlineKeyboardButton(text="Назад", callback_data=f"tag information {tag_name}")]
+    ])
+    return markup
+
+
+def back_to_settings(tag_name: str) -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Назад", callback_data=f"change_tag {tag_name}")]
+    ])
+    return markup
+
+
+def edit_users(tag_name: str) -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Добавить участников", callback_data=f"add_users {tag_name}")],
+        [InlineKeyboardButton(text="Удалить участников", callback_data=f"delete_users {tag_name}")],
+        [InlineKeyboardButton(text="Назад", callback_data=f"change_tag {tag_name}")]
+    ])
+    return markup
+
+
+def get_add_users(users: list, tag_name: str):
+    markup = InlineKeyboardMarkup(inline_keyboard=[[]])
+    for user in users:
+        markup.inline_keyboard.append([InlineKeyboardButton(text=f"@{user[0]}",
+                                                            callback_data=f"add_user {tag_name} {user[0]}")])
+    markup.inline_keyboard.append([InlineKeyboardButton(text="закончить выбор",
+                                                        callback_data=f"edit_users {tag_name}")])
+    return markup
+
+
+def get_delete_users(users: list, tag_name: str):
+    markup = InlineKeyboardMarkup(inline_keyboard=[[]])
+    for user in users:
+        markup.inline_keyboard.append([InlineKeyboardButton(text=f"@{user[0]}",
+                                                            callback_data=f"delete_user {tag_name} {user[0]}")])
+    markup.inline_keyboard.append([InlineKeyboardButton(text="закончить выбор",
+                                                        callback_data=f"edit_users {tag_name}")])
     return markup
