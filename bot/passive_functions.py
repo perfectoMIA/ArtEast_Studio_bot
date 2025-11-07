a5ca3de53de1c7c650820ec9a3ab5a9fa1a68cff
perfectoMIA <maremyanov.ia@students.dvfu.ru>
2025-11-07 11:29:58 +1000

    добавил функцию удаления всех сообщений в личке (пока не применил) и функцию рассылки о сборе денег на др

--- bot/passive_functions.py
+++ bot/passive_functions.py
@@ -4,6 +4,7 @@ from datetime import date, datetime, timedelta
 from aiogram import Bot
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
+import calendar
 
 from bot.models import DataBase
 from bot.keyboards import inline as inline_keyborads
@@ -19,15 +20,20 @@ async def birthday_notice(bot: Bot):
                 persons.pop(i)
             else:
                 i += 1
-        today = date.today()
+        today = datetime.today()
         admins_id = DataBase.Get_admins_id()
         for i in range(len(admins_id)):
             admins_id[i] = admins_id[i][0]
         for person in persons:
             birthday = datetime.strptime(person[1], "%d.%m.%Y")
-            if today.month == birthday.month and today.day + 7 == birthday.day:
+            birthday = birthday.replace(year=today.year)
+            if today.date() + timedelta(days=14) == birthday.date() or today.date() + timedelta(days=7) == birthday.date():
+                await asyncio.sleep((today.time() - time.strptime("00:00:00", "%H:%M:%S")).total_seconds)  # ДОРАБОТАТЬ
                 for ID in admins_id:
-                    await bot.send_message(chat_id=ID, text=f"через 7 дней день рождения у: @{person[0]}")
+                    print(f"через {(birthday.date() - today).days} дней день рождения у: @{person[0]}")
+                    #await bot.send_message(chat_id=ID, text=f"через {(birthday.date() - today.date()).days} "
+                    #                                        f"дней день рождения у: @{person[0]}")
+
         await asyncio.sleep(86400)  # раз в сутки запускается
 
 
@@ -77,4 +83,53 @@ async def spam_mailing(bot: Bot):
         else:
             next_day = now + timedelta(days=1)
             next_day = next_day.replace(hour=00, microsecond=0, second=0, minute=0)
-            await asyncio.sleep((next_day - now).seconds)
+            await asyncio.sleep((next_day - now).total_seconds())
+
+
+async def delete_message(bot: Bot):
+    message = await bot.send_message(chat_id=2081599417, text="СРАЗУ УДАЛЮ")
+    message_id = message.message_id
+    while True:
+        try:
+            await bot.delete_message(chat_id=2081599417, message_id=message_id)
+        except Exception as e:
+            print(type(e), e)
+            print(message_id)
+        message_id -= 1
+        if message_id == -1:
+            break
+
+
+# рассылка о сборе денег на др
+async def Spam_about_money(bot: Bot):
+    while True:
+        today = datetime.now()
+        if today.day == 7 and today.hour == 11:  # 6 числа в 12:00 по Владивостоку - это 2:00 по серверу
+            DataBase.Reset_sent_money()  # каждый месяц нужно заново скидывать деньги, поэтому сбарсываем флаг
+            users = DataBase.Get_users_ids()
+            for i in range(len(users)):
+                users[i] = users[i][0]
+            text = ("Привет! Каждый месяц мы собираем деньги на подарки для именинников нашей студии:\n\n"
+                    "*780 ₽*\n\n"
+                    "2202205349971173\n"
+                    "(Сбербанк, Светлана Анатольевна З.)\n\n"
+                    "Просьба скинуть чек в этот чат, он будет перенаправлен: @anleeli (Анна Ли)")
+
+            await bot.send_message(chat_id=2081599417, text=text, parse_mode="Markdown")
+
+            # for user in users:
+            #     await bot.send_message(chat_id=user, text=text, parse_mode="Markdown")
+            days_in_month = calendar.monthrange(today.year, today.month)[1]
+            next_spam_day = today + timedelta(days=days_in_month)
+            await asyncio.sleep((next_spam_day - today).total_seconds())
+        else:
+            if today.day == 7 and today.hour < 2 or today.day < 7:  # если я запущу бота и дата рассылки ещё не прошла
+                await asyncio.sleep(
+                   (datetime(year=today.year, month=today.month, day=7,
+                             hour=2, minute=0, second=0, microsecond=0) - today).total_seconds()
+                )
+            else:  # если я запущу бота и дата рассылки уже прошла
+                days_in_month = calendar.monthrange(today.year, today.month)[1]
+                spam_day = today + timedelta(days=days_in_month)
+                spam_day = spam_day.replace(day=7, hour=2, minute=0, second=0, microsecond=0)
+                await asyncio.sleep((spam_day - today).total_seconds())
